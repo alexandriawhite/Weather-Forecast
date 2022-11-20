d34c0719b484634d993ae26b98b33d2e2df22165
Alexandria <alexandriacwhite@comcast.net>
2022-11-20 16:49:57 -0600

    5 day forecast section is working and formatted with CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ let fiveDay = document.querySelector(".fiveDay");
 function weather() {
     let city = $("#userInput").val()
     let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=imperial`
+    let weatherURLToday = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`
 
     fetch(weatherURL)
         .then(function (response) {
@@ -16,9 +17,12 @@ function weather() {
         })
         .then(function (data) {
             displayForecast(data)
+            console.log(data)
             localStorage.setItem("city",city) /*Need to update this to an array*/
         })
 }
+
+
 
 
 
@@ -33,14 +37,21 @@ function displayForecast(forecastData) {
     })
    filterForecast.forEach(day => {
     let weatherCard = document.createElement("div")
-    let fiveDayWeather = document.createElement("h1")
+    let fiveDayWeather = document.createElement("h2")
     let symbol = document.createElement("img")
     let temp = document.createElement("p")
     let wind = document.createElement("p")
     let humidity = document.createElement("p")
-    fiveDayWeather.textContent= day.dt_txt.split(" ")[0]
+    let date = day.dt_txt.split(" ")[0]
+    let [year, month, dy] = date.split('-');
+    let formattedDate = [month, dy, year].join('/');
+    fiveDayWeather.textContent= formattedDate
     symbol.setAttribute("src",`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`)
-    weatherCard.append(fiveDayWeather,symbol)
+    temp.textContent= `Temp: ${day.main.temp}°F`
+    wind.textContent=`Wind: ${day.wind.speed}MPH`
+    humidity.textContent=`Humidity: ${day.main.humidity}`
+    weatherCard.classList="forecastCard"
+    weatherCard.append(fiveDayWeather,symbol,temp,wind,humidity)
     fiveDay.append(weatherCard)
     
    });
